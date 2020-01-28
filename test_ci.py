e627a6dfd16e01498a1988a1ef43cbe79e2a8de7
AustinGrey <corey.crapo@gmail.com>
2020-01-27 18:09:57 -0700

    Added pytest

--- test_ci.py
+++ test_ci.py
@@ -8,3 +8,21 @@ def test_health_check():
 # 1. Add two more tests here of your choice. I will explain the API.
 #    Make sure to verify the necessary info, e.g., status code, response data.
 # 2. Add "pytest" as integration testing script as part of Github Actions CI workflow (you've done this before!)
+
+def test_hello_world():
+    r = requests.get('http://localhost:8085/hello-world')
+    assert(r.status_code == 200)
+    assert(r.json()['content'] == 'Hello, Stranger!')
+
+def test_id_counter():
+    r1 = requests.get("http://localhost:8085/hello-world")
+    r2 = requests.get("http://localhost:8085/hello-world")
+    try:
+        r1 = r1.json()
+        r2 = r2.json()
+    except:
+        assert(False)
+
+    # print(r1)
+    # print(r2)
+    assert(r1['id'] + 1 == r2['id'])
